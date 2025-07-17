3d2a21ee942dc25f7f7ae8d3171e4a8733b027d8
Feng Li <lf8881737@hotmail.com>
2025-07-16 22:21:33 -0700

    Fix: Import dj_database_url in settings

--- acc_project/settings.py
+++ acc_project/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-
+import dj_database_url
 load_dotenv() # 读取 .env 文件中的变量
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
